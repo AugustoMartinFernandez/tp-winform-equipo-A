33ea12604fc1e80e39445c4e9a4e3ba7a1f533b7
Willy Phelan <phelanguille@gmail.com>
2026-09-08 18:35:42 -0300

    Agrego metodos de negocio y filtrado

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Dominio;
+
+namespace Negocio {
+    public class CategoriaNegocio {
+        public List<Categoria> listar() {
+
+            List<Categoria> lista = new List<Categoria>() ;
+
+            AccesoDatos datos = new AccesoDatos() ;
+
+            try {
+
+                datos.setearConsultas("SELECT Id, Descripcion FROM CATEGORIAS") ;
+                
+                datos.ejecutarLectura()  ;
+
+                while (datos.Lector.Read()) {
+
+                    Categoria aux = new Categoria() ;
+
+                    aux.Id = (int)datos.Lector["Id"] ;
+
+                    aux.Descripcion = (string)datos.Lector["Descripcion"] ;
+
+                    lista.Add(aux) ;
+                }
+
+                return lista ;
+            }
+
+            catch (Exception ex) {
+              
+            throw ex ; }
+
+            finally {
+
+                datos.cerrarConexion() ;
+            }
+        }
+    
+        public void agregar(Categoria nueva) 
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try 
+            {
+                datos.setearConsultas("INSERT INTO CATEGORIAS (Descripcion) VALUES ('" + nueva.Descripcion + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+            finally 
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Categoria categoria) 
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try 
+            {
+                datos.setearConsultas("UPDATE CATEGORIAS SET Descripcion = '" + categoria.Descripcion + "' WHERE Id = " + categoria.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+            finally 
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id) 
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try 
+            {
+                datos.setearConsultas("DELETE FROM CATEGORIAS WHERE Id = " + id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+            finally 
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
+        
---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Dominio;
+
+namespace Negocio {
+    public class MarcaNegocio {
+        public List<Marca> listar() {
+
+            List<Marca> lista = new List<Marca>() ;
+
+            AccesoDatos datos = new AccesoDatos() ;
+
+            try {
+
+                datos.setearConsultas("SELECT Id, Descripcion FROM MARCAS") ;
+
+                datos.ejecutarLectura() ;
+
+                while (datos.Lector.Read()) {
+
+                    Marca aux = new Marca() ;
+
+                    aux.Id = (int)datos.Lector["Id"] ;
+
+                    aux.Descripcion = (string)datos.Lector["Descripcion"] ;
+
+                    lista.Add(aux) ;
+                }
+
+                return lista ;
+            }
+
+            catch (Exception ex) {
+
+                throw ex ; }
+           
+            finally {
+
+                datos.cerrarConexion() ;
+            }
+        }
+
+        public void agregar(Marca nueva) {
+
+            AccesoDatos datos = new AccesoDatos() ;
+         
+            try {
+
+                datos.setearConsultas("INSERT INTO MARCAS (Descripcion) VALUES ('" + nueva.Descripcion + "')") ;
+
+                datos.ejecutarAccion() ;
+            }
+            
+            catch (Exception ex){
+                
+            throw ex ; }
+
+            finally{ 
+
+                datos.cerrarConexion() ;
+            }
+        }
+
+        public void modificar(Marca marca) {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsultas("UPDATE MARCAS SET Descripcion = '" + marca.Descripcion + "' WHERE Id = " + marca.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsultas("DELETE FROM MARCAS WHERE Id = " + id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
